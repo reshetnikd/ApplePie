924cab761246028564ce22cc86da91fc98baaa23
reshetnikd <dmitry.reshetnik@icloud.com>
2021-04-08 14:08:04 +0300

    Initial Commit

---
+++ ApplePie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApplePie
+//
+//  Created by Dmitry Reshetnik on 08.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
