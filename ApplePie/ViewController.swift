d3fc62746cc6e96191988c3ec5b93bae7cebc720
reshetnikd <dmitry.reshetnik@icloud.com>
2021-04-12 21:34:25 +0300

    Build the interface

--- ApplePie/ViewController.swift
+++ ApplePie/ViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var treeImageView: UIImageView!
+    @IBOutlet var correctWordLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
